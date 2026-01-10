6fc0207f51265d3669e34d71ba01b066916d581a
DMQ <duminqing@outlook.com>
2026-01-10 19:59:15 +0800

    Initial commit

---
+++ android_myt/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AndroidMytConfig(AppConfig):
+    name = 'android_myt'
---
+++ browser_bit/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BrowserBitConfig(AppConfig):
+    name = 'browser_bit'
