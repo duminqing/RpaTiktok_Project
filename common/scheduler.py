548699bfc6e9a2b1b751d970afcd8c525dfa50d0
DMQ <duminqing@outlook.com>
2026-01-12 22:24:18 +0800

    刷视频

--- common/scheduler.py
+++ common/scheduler.py
@@ -1,8 +1,5 @@
-import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.conf import settings
-
-logger = logging.getLogger(__name__)
 
 # 全球调度器实例
 scheduler = None
@@ -18,7 +15,7 @@ def start_scheduler():
         # 检查任务是否已经存在，避免重复添加
         if not scheduler.running:
             scheduler.start()
-        from common.task_executor import execute_tiktok_publishing_tasks
+        from common.task_executor import execute_tiktok_publishing_tasks, execute_tiktok_scrolling_tasks
         # 检查是否已有相同ID的任务
         if not scheduler.get_job('video_publishing_job'):
             scheduler.add_job(
@@ -27,19 +24,27 @@ def start_scheduler():
                 hour=2,
                 minute=0,
                 id='video_publishing_job',
-                name='每天凌晨3点执行视频发布的任务'
+                name='每天凌晨2点执行视频发布的任务'
             )
-            print("定时任务已添加: 每天凌晨3点执行视频发布")
-            logger.info("定时任务已添加: 每天凌晨3点执行视频发布")
+            print("定时任务已添加: 每天凌晨2点执行视频发布")
         else:
             print("定时任务已存在，无需重复添加")
-            logger.info("定时任务已存在，无需重复添加")
-
-
+        if not scheduler.get_job('scrolling_job'):
+            scheduler.add_job(
+                execute_tiktok_scrolling_tasks,  # 传递函数引用而不是执行函数
+                'cron',
+                hour='0,6',  # 每天0点和6点执行
+                minute=0,
+                id='scrolling_job',
+                name='每天0点和6点执行视频滚动任务'
+            )
+            print("定时任务已添加: 每天0点和6点执行视频滚动")
+        else:
+            print("定时任务已存在，无需重复添加")
 def stop_scheduler():
     """停止定时任务调度器"""
     global scheduler
     
     if scheduler and scheduler.running:
         scheduler.shutdown()
-        logger.info("定时任务调度器已关闭")
+        print("定时任务调度器已关闭")
