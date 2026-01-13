faa7e030cdf495e16ba3c645be207573fe7d57f4
DMQ <duminqing@outlook.com>
2026-01-13 08:30:51 +0800

    添加定时任务

--- common/scheduler.py
+++ common/scheduler.py
@@ -8,19 +8,19 @@ logger = logging.getLogger(__name__)
 scheduler = None
 
 
-# def execute_video_publishing_job():
-#     """每天凌晨3点执行的视频发布任务"""
-#     # 延迟导入，避免AppRegistryNotReady错误
-#     from common.task_executor import execute_tiktok_publishing_tasks
-#     print("开始执行定时视频发布任务...")
-#     logger.info("开始执行定时视频发布任务...")
-#     try:
-#         execute_tiktok_publishing_tasks()
-#         print("定时视频发布任务执行完成")
-#         logger.info("定时视频发布任务执行完成")
-#     except Exception as e:
-#         print(f"定时视频发布任务执行失败: {str(e)}")
-#         logger.error(f"定时视频发布任务执行失败: {str(e)}")
+def execute_video_publishing_job():
+    """每天凌晨3点执行的视频发布任务"""
+    # 延迟导入，避免AppRegistryNotReady错误
+    from common.task_executor import execute_tiktok_publishing_tasks
+    print("开始执行定时视频发布任务...")
+    logger.info("开始执行定时视频发布任务...")
+    try:
+        execute_tiktok_publishing_tasks()
+        print("定时视频发布任务执行完成")
+        logger.info("定时视频发布任务执行完成")
+    except Exception as e:
+        print(f"定时视频发布任务执行失败: {str(e)}")
+        logger.error(f"定时视频发布任务执行失败: {str(e)}")
 
 
 def start_scheduler():
@@ -34,16 +34,17 @@ def start_scheduler():
         # 检查任务是否已经存在，避免重复添加
         if not scheduler.running:
             scheduler.start()
-        from common.task_executor import execute_tiktok_publishing_tasks
+        
         # 检查是否已有相同ID的任务
         if not scheduler.get_job('video_publishing_job'):
             scheduler.add_job(
-                execute_tiktok_publishing_tasks,  # 传递函数引用而不是执行函数
+                execute_video_publishing_job,  # 传递函数引用而不是执行函数
                 'cron',
-                hour=2,
+                hour=3,
                 minute=0,
                 id='video_publishing_job',
-                name='每天凌晨3点执行视频发布的任务'
+                name='每天凌晨3点执行视频发布的任务',
+                misfire_grace_time=3600  # 设置错过执行的宽限时间为1小时
             )
             print("定时任务已添加: 每天凌晨3点执行视频发布")
             logger.info("定时任务已添加: 每天凌晨3点执行视频发布")
